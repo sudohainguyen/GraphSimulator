ceee0c31c0eba02bcd11b946fc1bb813c63e3b96
Quang Hai <quanghai.ng1512@gmail.com>
2018-04-07 09:17:43 +0700

    Add Node User control

---
+++ GraphSimulator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using GraphSimulator.User_Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphContainer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var node = new Node
+            {
+                X = e.GetPosition(GraphContainer).X,
+                Y = e.GetPosition(GraphContainer).Y
+            };
+
+            Canvas.SetLeft(node, node.X - node.Diameter / 2);
+            Canvas.SetTop(node, node.Y - node.Diameter / 2);
+
+            GraphContainer.Children.Add(node);
+        }
+    }
+}
---
+++ GraphSimulator/User_Controls/Node.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphSimulator.User_Controls
+{
+    /// <summary>
+    /// Interaction logic for Node.xaml
+    /// </summary>
+    public partial class Node : UserControl
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public bool IsSelected { get; set; }
+        
+        public char Identifier
+        {
+            get { return (char)GetValue(IdentifierProperty); }
+            set { SetValue(IdentifierProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Identifier.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IdentifierProperty =
+            DependencyProperty.Register("Identifier", typeof(char), typeof(Node), new PropertyMetadata('A'));
+
+
+        public Brush FillBrush
+        {
+            get { return (Brush)GetValue(FillBrushProperty); }
+            set { SetValue(FillBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FillBrushProperty =
+            DependencyProperty.Register("FillBrush", typeof(Brush), typeof(Node), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(50, 106, 169))));
+
+        // r 193, g 87, b 87
+                    
+        public int Diameter
+        {
+            get { return (int)GetValue(DiameterProperty); }
+            set { SetValue(DiameterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Radius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DiameterProperty =
+            DependencyProperty.Register("Diameter", typeof(int), typeof(Node), new PropertyMetadata(40));
+
+
+        public Brush StrokeBrush
+        {
+            get { return (Brush)GetValue(StrokeBrushProperty); }
+            set { SetValue(StrokeBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StrokeBrushProperty =
+            DependencyProperty.Register("StrokeBrush", typeof(Brush), typeof(Node), new PropertyMetadata(new SolidColorBrush(Color.FromRgb(0, 0, 0))));
+
+        public Node()
+        {
+            InitializeComponent();
+        }
+    }
+}
