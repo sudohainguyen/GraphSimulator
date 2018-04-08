69b73f5646032d28c1122cc0462af93d75e15508
Quang Hai <quanghai.ng1512@gmail.com>
2018-04-09 01:25:16 +0700

    Make Node dragable

--- GraphSimulator/User_Controls/Node.xaml.cs
+++ GraphSimulator/User_Controls/Node.xaml.cs
@@ -25,7 +25,7 @@ namespace GraphSimulator.User_Controls
         private static Brush UNSELECTED_FILL_BRUSH = new SolidColorBrush(Color.FromRgb(152, 198, 234));
         private static Brush SELECTED_FILL_BRUSH = new SolidColorBrush(Color.FromRgb(193, 87, 87));
         private static Brush UNSELECTED_STROKE_BRUSH = new SolidColorBrush(Color.FromRgb(0, 101, 189));
-        private static Brush SELECTED_STROKE_BRUSH = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        private static Brush SELECTED_STROKE_BRUSH = new SolidColorBrush(Color.FromRgb(183, 61, 61));
         private static Brush UNSELECTED_IDENTIFIER_BRUSH = new SolidColorBrush(Color.FromRgb(0, 0, 0));
         private static Brush SELECTED_IDENTIFIER_BRUSH = new SolidColorBrush(Color.FromRgb(255, 255, 255));
 
@@ -33,9 +33,13 @@ namespace GraphSimulator.User_Controls
         private Brush _fillBrush = UNSELECTED_FILL_BRUSH;
         private Brush _strokeBrush = UNSELECTED_STROKE_BRUSH;
         private Brush _identifierBrush = UNSELECTED_IDENTIFIER_BRUSH;
+        private bool _inDrag = false;
+        private Point _anchorPoint;
 
         public double X { get; set; }
         public double Y { get; set; }
+        //public double CanvasLeft => X - Diameter / 2;
+        //public double CanvasTop => Y - Diameter / 2;
         public bool IsSelected
         {
             get => _isSelected;
@@ -67,7 +71,7 @@ namespace GraphSimulator.User_Controls
             }
         }
 
-        public int Diameter => 40;
+        public static int Diameter => 40;
 
         public Brush StrokeBrush
         {
@@ -94,18 +98,51 @@ namespace GraphSimulator.User_Controls
             InitializeComponent();
         }
 
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Node_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                IsSelected = !IsSelected;
-            }
+            IsSelected = !IsSelected; 
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Node_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (_inDrag)
+            {
+                var curPoint = e.GetPosition(this.Parent as Panel);
+                Canvas.SetLeft(this, Canvas.GetLeft(this) + (curPoint.X - _anchorPoint.X));
+                Canvas.SetTop(this, Canvas.GetTop(this) + (curPoint.Y - _anchorPoint.Y));
+                _anchorPoint = curPoint;
+                X = _anchorPoint.X;
+                Y = _anchorPoint.Y;
+                e.Handled = true;
+            }
+        }
+
+        private void Node_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!_inDrag)
+            {
+                _anchorPoint = e.GetPosition(this.Parent as Panel);
+                CaptureMouse();
+                _inDrag = true;
+                e.Handled = true;
+            }
+        }
+
+        private void Node_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_inDrag)
+            {
+                ReleaseMouseCapture();
+                _inDrag = false;
+                e.Handled = true;
+            }
         }
     }
 }
