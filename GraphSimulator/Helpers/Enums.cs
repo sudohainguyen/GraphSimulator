14f102681ccb30e88cd23069d574e0c83e415424
Quang Hai <quanghai.ng1512@gmail.com>
2018-05-14 16:37:32 +0700

    App Enums

---
+++ GraphSimulator/Helpers/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSimulator.Helpers
+{
+    public enum RunningMode
+    {
+        ShowTheResult,
+        StepByStep,
+        Visualization
+    }
+
+    public enum Direction
+    {
+        None = 0,
+        OneWay,
+        OneWayReserved,
+        TwoWay
+    }
+
+    public enum Operation
+    {
+        ADD,
+        DELETE
+    }
+}
